835500904d9993c929724f8b14ec6e7c02c168c9
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-06-30 11:26:41 -0400

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sumNum = 0;
 
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return "ERROR";
+    }
+
+    
+
+    if (num1 < num2){
+        for (; num1 <= num2; num1++){
+            sumNum += num1;
+        }
+    }
+    else if (num1 > num2) {
+        for (; num1 >= num2; num1--){
+            sumNum += num1;
+        }
+    }
+
+    return sumNum;
 };
 
 // Do not edit below this line
