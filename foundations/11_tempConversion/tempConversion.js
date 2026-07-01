f13a74f3ab48df3eda7dbc44d33b68e9d774fe50
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-07-01 15:03:18 -0400

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const convertedNum = (num - 32) * (5/9);
+  const roundedNum = Math.round(convertedNum * 10) / 10;
+  return roundedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const convertedNum = (num * (9/5) + 32);
+  const roundedNum = Math.round(convertedNum * 10) / 10;
+  return roundedNum;
 };
 
 // Do not edit below this line
