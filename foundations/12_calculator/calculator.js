47739aa0b7ba954ae6ee6173193f458d74b58feb
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-07-31 08:56:35 -0400

    Finish 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr = []) {
+  const sum = arr.reduce((total, nextNum) => total + nextNum, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr = []) {
+  const product = arr.reduce((total, nextNum) => total * nextNum);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1){
+    return 1;
+  }
+
+  for (let i = 0; i < num; i++){    
+    return num * factorial(num - 1);
+  }
+  
 };
 
 // Do not edit below this line
