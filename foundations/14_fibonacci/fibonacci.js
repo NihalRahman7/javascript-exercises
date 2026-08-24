220e70a172da6e59ccad7bb782c2304cd94611ac
Nihal Rahman <161543137+NihalRahman7@users.noreply.github.com>
2026-08-24 19:48:39 -0400

    Finish 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (parseInt(number) === 0) return 0;
+    if (parseInt(number) < 0) return "OOPS";
 
+    let prev = 1;
+    let current = 1;
+
+    for (let i = 2; i < number; i++){
+        let next = prev + current;
+        prev = current;
+        current = next;
+    }
+
+    return current;
 };
 
 // Do not edit below this line
